436cb456ccdf441fe707bee0a1c8d3c51975fa15
TheA1R3 <Arustam21@gmail.com>
2020-06-22 13:15:25 +0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,181 @@
+// let name = 'Art';
+
+// let float = 12.3;
+
+// let number = 12;
+
+// let bool = true/false;
+
+// Object = {}
+
+// let array = [];
+
+// name = 12;
+
+// let names = [
+//     'Art',
+//     'Vlad',
+// ];
+
+// console.log(names[1]);
+
+
+///
+
+// names[1] = 'new';
+
+// console.log(names[1]);
+
+// let names = [1,2,3];
+
+
+// let ofices = {
+//     name: 'Art',
+//     age: 23,
+// }
+
+// console.log(ofices.name)
+
+// ofices.name = 'Vlad';
+
+// console.log(ofices.name);
+
+
+// let company = ['google', 'yandex', 'yahoo']
+// console.log(company)
+
+// let schooly = {
+//     name: 'a',
+//     nomber: 0,
+// }
+
+// let test = [{
+//     name: 'vlad',
+//     o: 'schooly',
+// }]
+// console.log(test[0].o)
+
+// let a = 'b';
+// let b = 3;
+
+// console.log(a*b);
+// console.log(a*b);
+
+
+// function sum(nevajno, number2) {
+//     let test =  nevajno + number2
+
+//     return test;
+// }
+// let result = sum(5, 2)
+//  console.log(test);
+
+
+
+// if(a === 'a') {
+//    console.log(1); 
+// }
+
+// if(a === 'b') {
+//     console.log('b')
+// }
+
+
+// if(a === 'a') {
+//    console.log(1); 
+// } else if(a === 'b') {
+//     console.log('b')
+// } else {
+//     console.log(2)
+// }
+
+
+// for (let index = 0; index < 10; index++) {
+//     if(index === 3) {
+//         console.log(index);
+//     }
+// }
+
+// let test = document.getElementById('info-title');
+
+// test.innerText = 'tes';
+
+// let test = document.getElementsByClassName('info-table-first-conteiner-page');
+
+// console.log(test[0])
+
+// test[0].innerText = 'jgg'
+
+// console.log(test[0]);
+
+// function del(one, two, operation) {
+//     const x = '*';
+//     const v = '/';
+//     const b = '+';
+//     const z = '-';
+//     let result;
+
+//     if (operation === x) {
+//      return   one * two;
+//     }
+
+//     if (operation === v) {
+//         return one / two;
+//     }
+
+//     if (operation === b) {
+//         return one + two;
+//     }
+
+//     if (operation === z) {
+//         return one - two;
+//     }
+
+//     switch(operation) {
+//         case x:
+//         result =  one * two
+//         break;
+
+//         case b:
+//         result = one + two
+//         break;
+        
+//         case v:
+//         result = one / two
+//         break;
+
+//         case z:
+//         result = one - two
+//         break;
+
+//     }
+
+//       return result;
+// }
+
+// const result = del(2,4, '*');
+
+// console.log(result)
+
+// console.log(this)
+
+// setInterval(function() {
+    
+// }, interval);
+
+// setTimeout(() => {
+//     console.log('hello');
+// }, 5000);
+
+// setInterval(function() {
+//     console.log('hello');
+// }, 1000);
+
+
+// const company = ['google', 'yandex', 'yahoo'];
+
+// for (let index = 0; index < company.length; index++){
+//     if (index !== 1) {
+//         console.log(company[index]);
+//     }
+// }
